68c4e8e40d5668725a4407d304517845d8895e0c
sreelekha-2 <sreelekhakutikanti@gmail.com>
2022-06-30 11:42:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-uykkea
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-uykkea)
---
+++ script.js
@@ -0,0 +1,28 @@
+let modal = document.getElementById('modal');
+
+let openBtn = document.getElementById('openBtn');
+
+let closeBtn = document.getElementById('closeBtn');
+
+let text = document.getElementById('text');
+
+let inputEle = document.getElementById('inputEle');
+
+function getModal() {
+  inputEle.value = text.textContent;
+  modal.classList.remove('modal-container');
+}
+
+function closeModal() {
+  modal.classList.add('modal-container');
+}
+
+function handler() {
+  text.textContent = inputEle.value;
+}
+
+openBtn.addEventListener('click', getModal);
+
+closeBtn.addEventListener('click', closeModal);
+
+inputEle.addEventListener('keyup', handler);
